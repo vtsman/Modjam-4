3a9786466100902cd5b69d469c3f9e24232979d8
vtsman <hokiespencer@gmail.com>
2014-05-15 16:11:57 -0400

    Getting git to sync

--- vtsman/vmcraft/GuiComputer.java
+++ vtsman/vmcraft/GuiComputer.java
@@ -15,42 +15,45 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
-public class GuiComputer extends GuiScreen
-{
-    private static final ResourceLocation field_147017_u = new ResourceLocation("textures/gui/container/generic_54.png");
-    /**
-     * window height is calculated with these values; the more rows, the heigher
-     */
-    private int inventoryRows;
-    private static final String __OBFID = "CL_00000749";
+public class GuiComputer extends GuiScreen {
 
-    public GuiComputer()
-    {
-        super();
-    }
-    
-    public void drawScreen(int par1, int par2, float par3)
-    {
-    	this.drawGuiBackgroundLayer();
-        this.drawGuiForegroundLayer();
-    }
+	protected int xSize = 176;
+	/** The Y size of the inventory window in pixels. */
+	protected int ySize = 166;
 
-    /**
-     * Draw the foreground layer for the GuiContainer (everything in front of the items)
-     */
-    protected void drawGuiForegroundLayer()
-    {
-       this.fontRendererObj.drawString("Computer", 8, 6, 4210752);
-    }
-    
-    protected void drawGuiBackgroundLayer()
-    {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        Minecraft.getMinecraft().getTextureManager().bindTexture(field_147017_u);
-        int k = (this.width) / 2;
-        int l = (this.height) / 2;
-        this.drawTexturedModalRect(0, 0, 0, 0, 100, 100);
-        System.out.println("here");
-        //this.drawGradientRect(0, 0, this.width, this.height, -1072689136, -804253680);
-    }
+	private static final ResourceLocation field_147017_u = new ResourceLocation(
+			"vmcraft", "textures/gui/computerGui.png");
+	/**
+	 * window height is calculated with these values; the more rows, the heigher
+	 */
+	private int inventoryRows;
+	private static final String __OBFID = "CL_00000749";
+
+	public GuiComputer() {
+		super();
+	}
+
+	public void drawScreen(int par1, int par2, float par3) {
+		this.drawGuiBackgroundLayer();
+		this.drawGuiForegroundLayer();
+	}
+
+	/**
+	 * Draw the foreground layer for the GuiContainer (everything in front of
+	 * the items)
+	 */
+	protected void drawGuiForegroundLayer() {
+		this.fontRendererObj.drawString("Computer", 8, 6, 4210752);
+	}
+
+	protected void drawGuiBackgroundLayer() {
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		Minecraft.getMinecraft().getTextureManager()
+				.bindTexture(field_147017_u);
+		int k = (this.width - xSize) / 2;
+		int l = (this.height - ySize) / 2;
+		this.drawTexturedModalRect(k, l, 0, 0, xSize, ySize);
+		// this.drawGradientRect(0, 0, this.width, this.height, -1072689136,
+		// -804253680);
+	}
 }
