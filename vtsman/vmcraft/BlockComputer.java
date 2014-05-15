bb42f54eda43778ffdbfcbe6281954bea18ac569
vtsman <hokiespencer@gmail.com>
2014-05-14 21:26:44 -0400

    Auto commit

--- vtsman/vmcraft/BlockComputer.java
+++ vtsman/vmcraft/BlockComputer.java
@@ -1,5 +1,10 @@
 package vtsman.vmcraft;
 
+import java.io.IOException;
+
+import org.jpc.emulator.PC;
+import org.jpc.j2se.VirtualClock;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
@@ -7,6 +12,11 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+
+import org.jpc.j2se.VirtualClock;
+import org.jpc.support.Clock;
+import org.jpc.support.DriveSet;
+import org.jpc.support.DriveSet.BootType;
 
 public class BlockComputer extends Block{
 
@@ -24,7 +34,13 @@ public class BlockComputer extends Block{
 	@Override
 	public boolean onBlockActivated(World w, int x, int y, int z, EntityPlayer p, int meta, float hX, float hY, float hZ)
     {
-		
+		PC pc;
+		try {
+			DriveSet ds = DriveSet.buildFromArgs(new String[]{"-boot", "cdrom", "-cdrom", "/Users/Spencer/Documents/SLU.iso"});
+			pc = new PC(new VirtualClock(), new String[]{""});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         return true;
     }
 
