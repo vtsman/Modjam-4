84aea6b415c1e3baf3895d45c462af69c29ddc7b
vtsman <hokiespencer@gmail.com>
2014-05-15 16:43:10 -0400

    Auto commit

--- vtsman/vmcraft/GuiComputer.java
+++ vtsman/vmcraft/GuiComputer.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiLabel;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.inventory.ContainerChest;
@@ -49,7 +50,10 @@ public class GuiComputer extends GuiScreen {
 	protected void drawGuiForegroundLayer() {
 		//this.fontRendererObj.drawString("Computer", 8, 6, 4210752);
 		//Minecraft.getMinecraft().renderEngine.getTexture(par1ResourceLocation)
+		int k = (this.width - xSize) / 2;
+		int l = (this.height - ySize) / 2;
 		TextureUtil.uploadTextureImage(TextureUtil.glGenTextures(), card.buffer);
+		this.drawTexturedModalRect(k - 4, l - 4, 0, 0, xSize + 4, ySize + 4);
 	}
 
 	protected void drawGuiBackgroundLayer() {
@@ -62,4 +66,17 @@ public class GuiComputer extends GuiScreen {
 		// this.drawGradientRect(0, 0, this.width, this.height, -1072689136,
 		// -804253680);
 	}
+	
+	 public void drawTexturedRect(int par1, int par2, int par3, int par4, int par5, int par6)
+	    {
+	        float f = 0.00390625F;
+	        float f1 = 0.00390625F;
+	        Tessellator tessellator = Tessellator.instance;
+	        tessellator.startDrawingQuads();
+	        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + par6), (double)this.zLevel, (double)((float)(par3 + 0) * f), (double)((float)(par4 + par6) * f1));
+	        tessellator.addVertexWithUV((double)(par1 + par5), (double)(par2 + par6), (double)this.zLevel, (double)((float)(par3 + par5) * f), (double)((float)(par4 + par6) * f1));
+	        tessellator.addVertexWithUV((double)(par1 + par5), (double)(par2 + 0), (double)this.zLevel, (double)((float)(par3 + par5) * f), (double)((float)(par4 + 0) * f1));
+	        tessellator.addVertexWithUV((double)(par1 + 0), (double)(par2 + 0), (double)this.zLevel, (double)((float)(par3 + 0) * f), (double)((float)(par4 + 0) * f1));
+	        tessellator.draw();
+	    }
 }
