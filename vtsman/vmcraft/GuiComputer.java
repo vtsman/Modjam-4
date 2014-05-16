6bd4d5815518f703147faaeaa6161d8642c83ef5
vtsman <hokiespencer@gmail.com>
2014-05-15 21:28:30 -0400

    Auto commit

--- vtsman/vmcraft/GuiComputer.java
+++ vtsman/vmcraft/GuiComputer.java
@@ -31,20 +31,14 @@ import org.lwjgl.opengl.GL11;
 public class GuiComputer extends GuiScreen {
 
 	protected int xSize = 176;
-	/** The Y size of the inventory window in pixels. */
 	protected int ySize = 166;
 
 	private static final ResourceLocation field_147017_u = new ResourceLocation(
 			"vmcraft", "textures/gui/computerGui.png");
-	/**
-	 * window height is calculated with these values; the more rows, the heigher
-	 */
-	private int inventoryRows;
-	private static final String __OBFID = "CL_00000749";
-	
 	DefaultVGACard card;
 	Keyboard kb;
 	EntityPlayer p;
+
 	public GuiComputer(DefaultVGACard vga, Keyboard kb, EntityPlayer p) {
 		super();
 		card = vga;
@@ -61,56 +55,95 @@ public class GuiComputer extends GuiScreen {
 	@Override
 	public void keyTyped(char par1, int par2) {
 		super.keyTyped(par1, par2);
-		if(this.isShiftKeyDown()){
+		if (this.isShiftKeyDown()) {
 			kb.keyPressed((byte) 0x36);
-		}
-		else{
+		} else {
 			kb.keyReleased((byte) 0x36);
 		}
-		if(this.isCtrlKeyDown()){
+		if (this.isCtrlKeyDown()) {
 			kb.keyPressed((byte) 0x1e);
-		}
-		else{
+		} else {
 			kb.keyReleased((byte) 0x1e);
 		}
-		
-		if('a' <= par1 && 'z' >= par1){
+
+		if ('a' <= par1 && 'z' >= par1) {
 			par1 += 'A' - 'a';
 		}
-		switch(par1){
-		case ':': par1 = ';'; break;
-		case '"': par1 = '\''; break;
-		case '<': par1 = ','; break;
-		case '>': par1 = '.'; break;
-		case '?': par1 = '/'; break;
-		case '|': par1 = '\\'; break;
-		case '{': par1 = '['; break;
-		case '}': par1 = ']'; break;
-		case '!': par1 = '1'; break;
-		case '@': par1 = '2'; break;
-		case '#': par1 = '3'; break;
-		case '$': par1 = '4'; break;
-		case '%': par1 = '5'; break;
-		case '^': par1 = '6'; break;
-		case '&': par1 = '7'; break;
-		case '*': par1 = '8'; break;
-		case '(': par1 = '9'; break;
-		case ')': par1 = '0'; break;
-		case '_': par1 = '-'; break;
-		case '+': par1 = '='; break;
+		switch (par1) {
+		case ':':
+			par1 = ';';
+			break;
+		case '"':
+			par1 = '\'';
+			break;
+		case '<':
+			par1 = ',';
+			break;
+		case '>':
+			par1 = '.';
+			break;
+		case '?':
+			par1 = '/';
+			break;
+		case '|':
+			par1 = '\\';
+			break;
+		case '{':
+			par1 = '[';
+			break;
+		case '}':
+			par1 = ']';
+			break;
+		case '!':
+			par1 = '1';
+			break;
+		case '@':
+			par1 = '2';
+			break;
+		case '#':
+			par1 = '3';
+			break;
+		case '$':
+			par1 = '4';
+			break;
+		case '%':
+			par1 = '5';
+			break;
+		case '^':
+			par1 = '6';
+			break;
+		case '&':
+			par1 = '7';
+			break;
+		case '*':
+			par1 = '8';
+			break;
+		case '(':
+			par1 = '9';
+			break;
+		case ')':
+			par1 = '0';
+			break;
+		case '_':
+			par1 = '-';
+			break;
+		case '+':
+			par1 = '=';
+			break;
 		}
-		
-		byte kc = KeyTable.getScancode(Integer.valueOf(KeyStroke.getKeyStroke(par1, 0).getKeyCode()));
-		System.out.println(par1 + ":" + KeyStroke.getKeyStroke(par1, 0).getKeyCode() + ":" + kc);
+
+		byte kc = KeyTable.getScancode(Integer.valueOf(KeyStroke.getKeyStroke(
+				par1, 0).getKeyCode()));
+		System.out.println(par1 + ":"
+				+ KeyStroke.getKeyStroke(par1, 0).getKeyCode() + ":" + kc);
 		kb.keyPressed(kc);
-		
-		if (updateMHz(markTime, totalExec)) 
-        {
-            markTime = System.currentTimeMillis();
-            totalExec = 0;
-        }
+
+		if (updateMHz(markTime, totalExec)) {
+			markTime = System.currentTimeMillis();
+			totalExec = 0;
+		}
 	}
-	
+
 	long markTime = System.currentTimeMillis();
 	int totalExec = 0;
 
@@ -126,7 +159,10 @@ public class GuiComputer extends GuiScreen {
 		TextureUtil
 				.uploadTextureImage(TextureUtil.glGenTextures(), card.buffer);
 		// this.drawTexturedModalRect(k - 4, l - 4, 0, 0, xSize + 4, ySize + 4);
-		this.drawTexturedModalRect(0, 0, 0, 0, this.width, this.height);
+		System.out.println(card.buffer.getWidth());
+		//this.drawTexturedRect(0, 0, 0, 0, this.width, this.height,
+		//		card.buffer.getWidth() / 2, card.buffer.getHeight() / 2);
+		this.drawTexturedRect(k - 4, l - 4, xSize + 4, ySize + 4);
 	}
 
 	protected void drawGuiBackgroundLayer() {
@@ -140,55 +176,49 @@ public class GuiComputer extends GuiScreen {
 		// -804253680);
 	}
 
-	public void drawTexturedRect(int par1, int par2, int par3, int par4,
-			int par5, int par6) {
+	public void drawTexturedRect(int par1, int par2, int par5, int par6) {
 		float f = 0.00390625F;
 		float f1 = 0.00390625F;
 		Tessellator tessellator = Tessellator.instance;
 		tessellator.startDrawingQuads();
 		tessellator.addVertexWithUV((double) (par1 + 0),
-				(double) (par2 + par6), (double) this.zLevel,
-				(double) ((float) (par3 + 0) * f),
-				(double) ((float) (par4 + par6) * f1));
+				(double) (par2 + par6), (double) this.zLevel, (double) (0),
+				(double) (1));
 		tessellator.addVertexWithUV((double) (par1 + par5),
-				(double) (par2 + par6), (double) this.zLevel,
-				(double) ((float) (par3 + par5) * f),
-				(double) ((float) (par4 + par6) * f1));
+				(double) (par2 + par6), (double) this.zLevel, (double) (1),
+				(double) (1));
 		tessellator.addVertexWithUV((double) (par1 + par5),
-				(double) (par2 + 0), (double) this.zLevel,
-				(double) ((float) (par3 + par5) * f),
-				(double) ((float) (par4 + 0) * f1));
+				(double) (par2 + 0), (double) this.zLevel, (double) (1),
+				(double) (0));
 		tessellator.addVertexWithUV((double) (par1 + 0), (double) (par2 + 0),
-				(double) this.zLevel, (double) ((float) (par3 + 0) * f),
-				(double) ((float) (par4 + 0) * f1));
+				(double) this.zLevel, (double) (0), (double) (0));
 		tessellator.draw();
 	}
-	
-	private static final int COUNTDOWN = 10000000;
-	
-	private boolean updateMHz(long time, long count)
-    {
-        long t2 = System.currentTimeMillis();
-        if (t2 - time < 100)
-            return false;
-        
-        count = COUNTDOWN - count;
-        float mhz = count * 1000.0F / (t2 - time) / 1000000;
 
-        float clockSpeed = 17.25F / 770 * mhz / 7.5F * 2.790F;
-        
-        //int percent = (int) (clockSpeed / 3000 * 1000 * 100 * 10);
-        Display.setTitle("Minecraft - VM running at " + mhz + " MHz");
-        return true;
-    }
-	
+	private static final int COUNTDOWN = 10000000;
+
+	private boolean updateMHz(long time, long count) {
+		long t2 = System.currentTimeMillis();
+		if (t2 - time < 100)
+			return false;
+
+		count = COUNTDOWN - count;
+		float mhz = count * 1000.0F / (t2 - time) / 1000000;
+
+		float clockSpeed = 17.25F / 770 * mhz / 7.5F * 2.790F;
+
+		// int percent = (int) (clockSpeed / 3000 * 1000 * 100 * 10);
+		Display.setTitle("Minecraft - VM running at " + mhz + " MHz");
+		return true;
+	}
+
 	@Override
-	public void onGuiClosed(){
+	public void onGuiClosed() {
 		Display.setTitle("Minecraft");
 	}
-	
+
 	@Override
-	protected void mouseClicked(int par1, int par2, int par3){
+	protected void mouseClicked(int par1, int par2, int par3) {
 		super.mouseClicked(par1, par2, par3);
 		kb.putMouseEvent(par1, par2, 0, par3);
 	}
