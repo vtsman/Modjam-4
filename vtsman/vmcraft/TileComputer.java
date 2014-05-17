ace0669b06e7c8fa72d9cda2a87d4c904790bfd3
vtsman <hokiespencer@gmail.com>
2014-05-16 22:52:12 -0400

    Auto commit

---
+++ vtsman/vmcraft/TileComputer.java
@@ -0,0 +1,20 @@
+package vtsman.vmcraft;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileComputer extends TileEntity{
+	public TileComputer(){
+		this.
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound t){
+		
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound t){
+		
+	}
+}
